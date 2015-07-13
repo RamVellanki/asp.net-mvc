1196efa9866e5746ddc5253f9a5111390d2208ef
chandravellanki <saroja9#>
2015-07-13 08:28:51 +0530

    Updated few more projects

---
+++ KOMVCApp/KOMVCApp/Models/ProductModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace KOMVCApp.Models
+{
+    public class ProductModel
+    {
+        [DisplayName("Product Id")]
+        public int ProdId { get; set; }
+
+        [DisplayName("Product Name")]
+        public string ProdName { get; set; }
+
+        [DisplayName("Product Owner")]
+        public string ProdOwner { get; set; }
+
+        [DisplayName("Number of Versions")]
+        public int NumOfVersions { get; set; }
+    }
+}
